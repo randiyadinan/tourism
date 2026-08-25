b86513398994345ec69a0b543a7dfc4350b932d1
randiyadinan <randiyadinan@gmail.com>
2026-08-26 01:19:09 +0530

    Configure Neon PostgreSQL Prisma connection and update frontend production URL

--- server/db.js
+++ server/db.js
@@ -1,14 +1,7 @@
 require("dotenv").config();
 
 const { PrismaClient } = require("@prisma/client");
-const { PrismaPg } = require("@prisma/adapter-pg");
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-
-const prisma = new PrismaClient({
-  adapter,
-});
+const prisma = new PrismaClient();
 
 module.exports = prisma;
