c0353b1b811e7513386f5a4e54c91a814407a65a
randiyadinan <randiyadinan@gmail.com>
2026-08-27 20:12:44 +0530

    Add api/[...all].ts catchall serverless function for full route handling

---
+++ api/[...all].ts
@@ -0,0 +1,15 @@
+import { Hono } from 'hono';
+import { handle } from 'hono/vercel';
+import app from '../server/src/index.js';
+
+export const config = {
+  runtime: 'edge'
+};
+
+const rootRouter = new Hono();
+
+// Mount under all possible Vercel serverless function incoming path variations
+rootRouter.route('/api', app);
+rootRouter.route('/', app);
+
+export default handle(rootRouter);
