04ebd4f68cc69acfe9ec1f9fb0330be2e4097ea1
randiyadinan <randiyadinan@gmail.com>
2026-08-28 02:34:56 +0530

    fix(vercel): route all /api/* requests to single serverless function api/index.ts

--- api/[...all].ts
+++
@@ -1,8 +0,0 @@
-import { getRequestListener } from '@hono/node-server';
-import app from '../server/src/index.js';
-
-export const config = {
-  runtime: 'nodejs'
-};
-
-export default getRequestListener(app.fetch);
